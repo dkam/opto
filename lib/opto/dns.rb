18b635a7e092e66cde4bf741f897d1d20227694e
Dan Milne <d@nmilne.com>
2016-01-12 16:33:46 +1100

    When looking at reverse DNS matching, check for CNames too

--- lib/opto/dns.rb
+++ lib/opto/dns.rb
@@ -21,21 +21,29 @@ class Dns
   end
 
   def check_reverse_mapping
-    ip_address = Resolv.getaddress(@server.host)
+    Resolv::DNS.open do |dns|
+      res = dns.getresources @server.host, Resolv::DNS::Resource::IN::ANY
 
-    begin 
-      rev_name = Resolv.getname(ip_address)
-    rescue Resolv::ResolvError => e
-      @result.warned("No reverse resolution for IP #{ip_address}")
+      # Does the host resolve to an CNAME?
+      if !res.select {|r| r.is_a? Resolv::DNS::Resource::IN::CNAME}.empty?
+        cname = res.first.name.to_s
+      end
+
+      ip_address = Resolv.getaddress(@server.host)
+
+      begin 
+        rev_name = Resolv.getname(ip_address)
+      rescue Resolv::ResolvError => e
+        @result.warned("No reverse resolution for IP #{ip_address}")
+      end
+
+      if @server.host != rev_name && rev_name != cname
+        @result.failed( "DNS: Your host doesn't have a matching reverse DNS entry (#{@server.host} != #{rev_name}")
+      elsif @server.host != rev_name && rev_name == cname
+        @result.passed( "DNS: Given host is a CName for which reverse resolution matches")
+      else
+        @result.passed( "DNS: Reverse resolution matches")
+      end
     end
-
-    if @server.host != rev_name
-      @result.failed( "DNS: Your host doesn't have a matching reverse DNS entry")
-      #puts "#{@server.host} ->  #{ip_address}"
-      #puts "#{ip_address}   ->  #{rev_name}"
-    else
-      @result.passed( "DNS: Reverse resolution matches")
-    end
-
   end
 end

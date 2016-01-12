9204ad28bf835010d27b430ebdb27cd92f3a9d88
Dan Milne <d@nmilne.com>
2016-01-12 14:31:26 +1100

    Calcualte page size

--- lib/opto/html.rb
+++ lib/opto/html.rb
@@ -20,6 +20,7 @@ class Html
   def check
     check_canonical
     check_ssb
+    check_size
   end
 
 
@@ -32,10 +33,17 @@ class Html
     return if canonical_element.nil?
 
     canonical_url = canonical_element.value
-    
-    @data.passed("HTML: Canonical URL matches URL") if  @data.raw_url ==  canonical_url
-    @data.warned("HTML: No Canonical URL") if  canonical_url.nil?
-    @data.failed("HTML: Canonical URL (#{canonical_url}) doesn't match given URL (#{@data.url.to_s})") if canonical_url != @data.raw_url
+
+    if  canonical_url.nil?
+      @data.warned("HTML: No Canonical URL") 
+    elsif URI.parse(canonical_url) == URI.parse(@data.raw_url)
+    #elsif  @data.raw_url ==  canonical_url
+      #if canonical_url != @data.raw_url
+      #if URI.parse(canonical_url) != URI.parse(@data.raw_url)
+      @data.passed("HTML: Canonical URL matches URL") 
+    else
+      @data.failed("HTML: Canonical URL (#{canonical_url}) doesn't match given URL (#{@data.raw_url})") 
+    end
   end
 
   ## 
@@ -83,5 +91,55 @@ class Html
     @data.failed("HTML: SSB params do not match") unless  query_param == target_param
 
 
+  end
+
+  ##
+  # Check page size
+  # Note: Currently does not take compression into account
+  ##
+  def check_size
+    c = HTTPClient.new
+    ##
+    # The downloaded page size
+    ##
+    #comp_get = c.head(@data.url)
+    #byebug
+    #compressed_page = comp_get.headers["Content-Length"].to_i if comp_get.status == 200
+    @data.passed("HTML: Page size #{@data.data.length.to_human} ") # (#{compressed_page.to_human} compressed)")
+
+    ##
+    # Use this base
+    ##
+    base = URI.parse("#{@data.url.scheme}://#{@data.url.host}")
+
+    ## 
+    #  Sum the Javascript scripts
+    #  Note: Need to take into account Google Analytics building a script tag that
+    #        this static analysis doesn't consider
+    ##
+    js_length = @data.doc.xpath('//script/@src').inject(0) do |sum, src|  
+      res = base.merge(URI.parse(src))
+      sum + c.head(res.to_s).headers["Content-Length"].to_i
+    end
+
+    ## This will produce incorrect results if the server doesn't support gz
+    cjs_length = @data.doc.xpath('//script/@src').inject(0) do |sum, src|  
+      res = base.merge(URI.parse(src))
+      sum + c.head(res.to_s + ".gz").headers["Content-Length"].to_i
+    end
+    @data.passed("HTML: Javascript size #{js_length.to_human} (Compressed: #{cjs_length.to_human})")
+
+    ##
+    # Sum the CSS
+    ##
+    cs_length = @data.doc.xpath('//link/@href').select{|s| s.value[/css$/]}.inject(0) do |sum, src|  
+      res = base.merge(URI.parse(src))
+      sum + c.head(res.to_s).headers["Content-Length"].to_i
+    end
+    ccs_length = @data.doc.xpath('//link/@href').select{|s| s.value[/css$/]}.inject(0) do |sum, src|  
+      res = base.merge(URI.parse(src))
+      sum + c.head(res.to_s + ".gz").headers["Content-Length"].to_i
+    end
+    @data.passed("HTML: Style size #{cs_length.to_human}")
   end
 end

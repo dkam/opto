a03d5a69f0c067b6a41da15f04ef9365db185d8f
Dan Milne <d@nmilne.com>
2016-03-01 10:03:07 +1100

    Make it more readable

--- lib/opto.rb
+++ lib/opto.rb
@@ -117,7 +117,7 @@ class Numeric
     units = %w{B KB MB GB TB}
     return 0 if self == 0
     e = (Math.log(self)/Math.log(1024)).floor
-    s = "%.3f" % (to_f / 1024**e)
+    s = "%.3f " % (to_f / 1024**e)
     s.sub(/\.?0*$/, units[e])
   end
 end

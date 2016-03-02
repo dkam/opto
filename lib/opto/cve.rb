c8b35b8e9046aaca0cec3c72c9fb276be7d90389
Dan Milne <d@nmilne.com>
2016-03-02 11:25:48 +1100

    Update CVE checking to work with per-software cve files

--- lib/opto/cve.rb
+++ lib/opto/cve.rb
@@ -8,20 +8,27 @@ class Cve < Checker
   description         'Check for CVE'
   supported_protocols true
 
+  DATA_BASE = File.expand_path("~/.opto/data")
 
+
+  ##
+  #  Currently only checking server
+  ##
   def checks
     return if  @server.info[:server_version].nil? && @server.info[:app_server_version].nil? 
 
-    cve_data = update_cve_files
+    cve_data = update_cve_files(@server.info[:server_name])
 
     if cve_data.nil?
-      @result.warned("CVE Data not available.")
+      @result.warned("CVE Data not available for #{@server.info[:server_name]}.")
       return
     end
 
     server_version = @server.info[:server_version] 
 
-    cve_data.dig(@server.info[:server_name], "cves").each do |cve|
+    short_name = "#{@server.info[:server_name]}/#{@server.info[:server_version]}"
+
+    cve_data["cves"].each do |cve|
       from_v  = Version.new cve['from']
       to_v    = cve['to'].nil?    ? nil : Version.new(cve['to']) 
       prior_v = cve['prior'].nil? ? nil : Version.new(cve['prior']) 
@@ -31,7 +38,7 @@ class Cve < Checker
         
         #  if there are no to_ / prior_v or  to_v is present and we're in the range     or  prior is present and we're in the range, then...
         if ( to_v.nil? && prior_v.nil? ) || ( !to_v.nil? && server_version <= to_v ) || ( !prior_v.nil? && server < prior_v )
-          @result.failed("CVE-#{cve['cve']}: #{cve['title']}: #{cve['url']}")
+          @result.failed("CVE: #{short_name} CVE-#{cve['cve']}: #{cve['title']}: #{cve['url']}")
         end
       end
 
@@ -39,42 +46,56 @@ class Cve < Checker
 
   end
 
+  private
+  def cve_data(server)
+    JSON.parse( File.read( File.join( DATA_BASE, "#{server}.json") ) )
+  rescue JSON::ParserError => e
+    puts "Parse error for CVE data for #{server}"
+    return nil
+  rescue
+    nil
+  end
 
+  def cve_etag(server)
+    File.read( File.join( DATA_BASE, "#{server}.etag") )
+  rescue
+    nil
+  end
 
-  def update_cve_files
-    base = File.expand_path("~/.opto")
-    etag_file = File.join( base, 'cve.etag')
-    data_file = File.join( base, 'cve.json')
-    data_url = "https://raw.githubusercontent.com/dkam/opto/master/cve.json"
+  def store_cve(server, data, etag)
+    File.write(File.join( DATA_BASE, "#{server}.json"), data)
+    File.write(File.join( DATA_BASE, "#{server}.etag"), etag)
+  end
 
-    Dir.mkdir(base) unless Dir.exist?( base )
+  def update_cve_files(server)
 
-    etag = File.exist?(etag_file) ? File.read(etag_file) : nil
+    FileUtils.mkdir_p(DATA_BASE) unless Dir.exist?( DATA_BASE )
+
+    data_url = "https://raw.githubusercontent.com/dkam/opto/master/data/cve/#{server}.json"
+
+    etag = cve_etag(server)
+    json_data = cve_data(server)
 
     # Set the eTag header if we have an etag file.  
     # Don't set the header if we don't have the data file - we need to download it anyway
     
     headers = {}
-    headers["If-None-Match"] = etag if ! etag.nil? && File.exist?(data_file)
+    headers["If-None-Match"] = etag if !etag.nil? && !json_data.nil?
 
     begin 
       data = open(data_url, headers)
-
-      File.write(etag_file, data.meta['etag'])
-      File.write(data_file, data.read)
-
+      store_cve(server, data.read, data.meta['etag'])
     rescue OpenURI::HTTPError => e
-      puts "CVE Data up-to-date"
+      puts  e.io.status[0] 
+      if  e.io.status[0] == "404"
+        puts "CVE data for #{server} not found"
+      elsif  e.io.status[0] == "304"
+        puts "CVE Data already up-to-date"
+        puts "#{e.message}"
+      end
     end
 
-    begin 
-      doc  = JSON.parse(File.read(data_file))
-
-    rescue JSON::ParserError => e
-      puts "Parse error for CVE data ~/.opto/cve.json"
-      return nil
-    end
-    return doc 
+    return cve_data(server) 
   end
 
 end

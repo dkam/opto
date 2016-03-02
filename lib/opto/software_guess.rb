37e01cdbecafce86a093bbdfef8b5cf067bcd7ac
Dan Milne <d@nmilne.com>
2016-03-02 11:26:14 +1100

    Lowercase server names, recongise IIS

--- lib/opto/software_guess.rb
+++ lib/opto/software_guess.rb
@@ -30,16 +30,23 @@ class SoftwareGuess < Checker
       # Now see if we recognise it
       ## 
       if server =~ /nginx/i 
-        @server.info[:server_name] = 'Nginx' 
+        @server.info[:server_name] = 'nginx' 
         server_version = server[/\/([\d\.]*)/, 1]
         @server.info[:server_version] = Version.new(server_version) if server_version
       end
 
-      @server.info[:server_name] = 'Ruby Thin' if server =~ /\Athin\z/i 
+      @server.info[:server_name] = 'ruby thin' if server =~ /\Athin\z/i 
 
       #"server" => "Apache/2.2.15 (CentOS)",
       if server =~ /Apache/i
-        @server.info[:server_name] = 'Apache' 
+        @server.info[:server_name] = 'apache' 
+        server_version = server[/\/([\d\.]*)/, 1]
+        @server.info[:server_version] = Version.new(server_version) if server_version
+      end
+
+      # "server" => "Microsoft-IIS/8.5"
+      if server =~ /Microsoft-IIS/i
+        @server.info[:server_name] = 'microsoft-iis' 
         server_version = server[/\/([\d\.]*)/, 1]
         @server.info[:server_version] = Version.new(server_version) if server_version
       end
@@ -60,25 +67,25 @@ class SoftwareGuess < Checker
       @server.info[:app_server_name] = powered_by
 
       if powered_by =~ /Phusion Passenger/ 
-        @server.info[:app_server_name] = 'Phusion Passenger'
+        @server.info[:app_server_name] = 'phusion passenger'
         @server.info[:app_server_version] =  Version.new(powered_by[/([\d\.]+)/, 1])
       end
 
       # "x-powered-by" => "PHP/5.5.9-1ubuntu4.14",
       if powered_by =~ /\APHP/ 
-        @server.info[:app_server_name] = 'PHP'
+        @server.info[:app_server_name] = 'php'
         @server.info[:app_server_version] =  Version.new(powered_by[/([\d\.]+)/, 1])
       end
     end
 
     if @server.response.headers['x-rack-cache']
-      @server.info[:app_server_name] = 'Ruby Rack'
+      @server.info[:app_server_name] = 'ruby rack'
     end
 
     #"x-generator" => "Drupal 7 (http://drupal.org)",
     if generator = @server.response.headers["x-generator"] 
       if generator =~ /Drupal/
-        @server.info[:app_server_name] = 'Drupal'
+        @server.info[:app_server_name] = 'drupal'
         @server.info[:app_server_version] =  Version.new(generator[/([\d\.]+)/, 1])
       end
     end
@@ -91,27 +98,28 @@ class SoftwareGuess < Checker
   def guess_os
     if powered_by = @server.response.headers['x-powered-by']
       if powered_by =~ /ubuntu/
-        @server.info[:server_os] = 'Ubuntu'
+        @server.info[:server_os] = 'ubuntu'
+        @server.info[:server_os_version] = powered_by[/ubuntu(\d*\.\d*)/,1]
       end
     end
     if server = @server.response.headers["server"]
       if server =~ /CentOS/
-        @server.info[:server_os] = 'CentOS'
+        @server.info[:server_os] = 'centos'
       end
     end
 
-    @result.info("Server OS is #{@server.info[:server_os]}") if @server.info[:server_os]
+    @result.info("Server OS is #{@server.info[:server_os]} / #{@server.info[:server_os_version]||'(unknown version)'}") if @server.info[:server_os]
   end
 
   def guess_site
-    @server.info[:site] = "Shopify" if @server.response.headers['x-shopid']
-    @server.info[:site] = "Github" if @server.response.headers['server'] == "GitHub.com"
+    @server.info[:site] = "shopify" if @server.response.headers['x-shopid']
+    @server.info[:site] = "github" if @server.response.headers['server'] == "GitHub.com"
   end
 
   def guess_application
     if element = @server.response.doc.at_xpath("//div[@class='site-info']")
       if element.text.strip =~ /Proudly powered by WordPress/
-        @server.info[:application] = 'Wordpress'
+        @server.info[:application] = 'wordpress'
       end
     end
     @result.info("Application is #{@server.info[:application]}") if @server.info[:application]
